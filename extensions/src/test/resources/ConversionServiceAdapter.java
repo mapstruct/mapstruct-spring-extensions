07841869984ceae5be204882a53bbd1aa2ab4231
Raimund Klein <770876+Chessray@users.noreply.github.com>
2021-01-08 23:08:30 +0100

    Prefer constructor injection over field injection.
    Fixes #13

--- extensions/src/test/resources/ConversionServiceAdapter.java
+++ extensions/src/test/resources/ConversionServiceAdapter.java
@@ -1,22 +1,23 @@
 package org.mapstruct.extensions.spring.converter;
 
 import javax.annotation.Generated;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Component;
 import test.Car;
 import test.CarDto;
 
 @Generated(
-        value = "org.mapstruct.extensions.spring.converter.ConversionServiceAdapterGenerator",
-        date = "2020-03-29T15:21:34.236Z"
-)
+    value = "org.mapstruct.extensions.spring.converter.ConversionServiceAdapterGenerator",
+    date = "2020-03-29T15:21:34.236Z")
 @Component
 public class ConversionServiceAdapter {
-    @Autowired
-    private ConversionService conversionService;
+  private final ConversionService conversionService;
 
-    public CarDto mapCarToCarDto(final Car source) {
-        return conversionService.convert(source, CarDto.class);
-    }
+  public ConversionServiceAdapter(final ConversionService conversionService) {
+    this.conversionService = conversionService;
+  }
+
+  public CarDto mapCarToCarDto(final Car source) {
+    return conversionService.convert(source, CarDto.class);
+  }
 }
