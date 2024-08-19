43581b8e15b61354d4190d33dd6ea994a714ca89
Jeff Schnitzer <jeff@infohazard.org>
2024-08-18 23:11:59 -0700

    Replace generated javax.annotation.PostConstruct with jakarta (#110)
    * Replace generated javax.annotation.PostConstruct with jakarta.annotation.PostConstruct
    
    * Auto detect jakarta vs javax PostConstruct
    
    * Use annotation processing mechanism to discover PostConstruct package
    
    * Add a test for jakarta PostConstruct
    
    ---------
    
    Co-authored-by: Jeff Schnitzer <jeff.schnitzer@hardrockdigital.com>

---
+++ extensions/src/test/resources/ConverterRegistrationConfigurationNoGeneratedJakartaAnnotation.java
@@ -0,0 +1,26 @@
+package org.mapstruct.extensions.spring.converter;
+
+import jakarta.annotation.PostConstruct;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.core.convert.support.ConfigurableConversionService;
+
+@Configuration
+class ConverterRegistrationConfiguration {
+  private final ConfigurableConversionService myConversionService;
+  private final List<Converter<?, ?>> converters;
+
+  ConverterRegistrationConfiguration(
+      @Qualifier("myConversionService") final ConfigurableConversionService myConversionService,
+      final List<Converter<?, ?>> converters) {
+    this.myConversionService = myConversionService;
+    this.converters = converters;
+  }
+  
+  @PostConstruct
+  void registerConverters() {
+    converters.forEach(myConversionService::addConverter);
+  }
+}
