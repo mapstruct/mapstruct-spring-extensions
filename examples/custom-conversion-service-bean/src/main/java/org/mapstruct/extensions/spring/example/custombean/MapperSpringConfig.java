41c5d303401b27c92739d67a1be4f82a226d49ec
denis.simonov <denis.simonov@ge.com>
2025-01-29 10:22:05 +0200

    reverted mistakenly committed change

--- examples/custom-conversion-service-bean/src/main/java/org/mapstruct/extensions/spring/example/custombean/MapperSpringConfig.java
+++ examples/custom-conversion-service-bean/src/main/java/org/mapstruct/extensions/spring/example/custombean/MapperSpringConfig.java
@@ -4,6 +4,6 @@ import org.mapstruct.MapperConfig;
 import org.mapstruct.extensions.spring.SpringMapperConfig;
 
 @MapperConfig(componentModel = "spring", uses = ConversionServiceAdapter.class)
-@SpringMapperConfig(conversionServiceBeanName = "myConversionService", generateConverterScan = true)
+@SpringMapperConfig(conversionServiceBeanName = "myConversionService")
 public interface MapperSpringConfig {
 }
